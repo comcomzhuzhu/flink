90b2149e5129d1e553f4d9e24a1c3f5759c70afa
Zxing <comcomzhuzhu@163.com>
2021-04-14 11:18:21 +0800

    commit

--- flink-test/src/main/scala/com/atguigu/day5work/Work1.scala
+++ flink-test/src/main/scala/com/atguigu/day5work/Work1.scala
@@ -3,38 +3,39 @@ package com.atguigu.day5work
 import java.sql.{Connection, DriverManager, PreparedStatement}
 import java.util.Properties
 
+import org.apache.flink.api.common.functions.ReduceFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.configuration.Configuration
-import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
-import org.apache.flink.util.Collector
 
-/**
-  * @ObjectName Work1
-  * @Description TODO
-  * @Author Xing
-  * @Date 2021/4/14 8:39
-  * @Version 1.0
-  */
 object Work1 {
   def main(args: Array[String]): Unit = {
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(3)
     val pro = new Properties()
     pro.put("bootstrap.server", "hadoop102:9092")
     val kafkaDS: DataStream[String] = env.addSource(new FlinkKafkaConsumer[String]("first", new SimpleStringSchema(), pro))
+    env.socketTextStream("hadoop102", 1589)
+      .flatMap(_.split(" ")).map((_, 1L))
+      .keyBy(_._1).reduce(new ReduceFunction[(String, Long)] {
+      override def reduce(value1: (String, Long), value2: (String, Long)): (String, Long) = {
+        (value1._1,value1._2+1)
+      }
+    }).print("xx")
 
-    kafkaDS.flatMap(_.split(" ")).map((_, 1L))
-      .keyBy(t => t)
-      .process(new ProcessFunction[(String, Long), (String, Long)] {
-        var count: Long = 0L
 
-        override def processElement(value: (String, Long), ctx: ProcessFunction[(String, Long), (String, Long)]#Context, out: Collector[(String, Long)]): Unit = {
-          count += 1L
-          out.collect((value._1, count))
-        }
-      }).addSink(new SinkDay05)
+//           不能使用一个  val count = 0L 因为这个count是每个并行度共享
+//      .process(new ProcessFunction[(String, Long), mutable.HashMap[String, Long]] {
+//        private val map = new mutable.HashMap[String, Long]()
+
+//        override def processElement(value: (String, Long), ctx: ProcessFunction[(String, Long), mutable.HashMap[String, Long]]#Context, out: Collector[mutable.HashMap[String, Long]]): Unit = {
+//          map(value._1) = map.getOrElse(value._1, 0l) + 1L
+//          out.collect(map)
+//        }
+//      }).print("test")
+
 
     env.execute()
   }
@@ -44,8 +45,8 @@ object Work1 {
     var pst: PreparedStatement = _
 
     override def invoke(value: (String, Long), context: SinkFunction.Context): Unit = {
-      pst.setString(1,value._1)
-      pst.setLong(2,value._2)
+      pst.setString(1, value._1)
+      pst.setLong(2, value._2)
       pst.execute()
     }
 
