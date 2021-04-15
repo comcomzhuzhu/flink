d4f28fa916ae221da0ea046b4e5f53dc73b40128
Zxing <comcomzhuzhu@163.com>
2021-04-15 14:26:19 +0800

    commit

--- flink-test/src/main/java/com/atguigu/statetest/State_KeyedApp.java
+++ flink-test/src/main/java/com/atguigu/statetest/State_KeyedApp.java
@@ -2,9 +2,19 @@ package com.atguigu.statetest;
 
 import com.atguigu.apitest.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.io.IOException;
+
 
 /**
  * @ClassName State_KeyedApp
@@ -22,8 +32,51 @@ public class State_KeyedApp {
             return new SensorReading(fields[0], Long.valueOf(fields[1]), Double.valueOf(fields[2]));
         });
 
+        SingleOutputStreamOperator<Tuple3<String, Double, Double>> warningDS = dataDS.keyBy(SensorReading::getId)
+                .flatMap(new TempChangeWarning(10.0));
 
-        
+
+        warningDS.print();
         env.execute();
+    }
+
+    public static class TempChangeWarning extends RichFlatMapFunction<SensorReading, Tuple3<String, Double, Double>> {
+
+        private Double threshold;
+
+        //        保存上一次的状态
+        private ValueState<Double> lastTemp;
+
+        @Override
+        public void open(Configuration parameters) {
+            lastTemp = getRuntimeContext().getState(
+                    new ValueStateDescriptor<>("lastTemp", Double.class)
+            );
+        }
+
+        @Override
+        public void close() {
+            lastTemp.clear();
+        }
+
+        public TempChangeWarning(Double threshold) {
+            this.threshold = threshold;
+        }
+
+
+        @Override
+        public void flatMap(SensorReading value, Collector<Tuple3<String, Double, Double>> out) throws IOException {
+//             获取之前存的状态
+            Double lastTempD = lastTemp.value();
+            if (lastTempD != null) {
+                Double diff = Math.abs(value.getTemperature() - lastTempD);
+                if (diff>=threshold){
+                    out.collect(Tuple3.of(value.getId(),lastTempD,value.getTemperature()));
+                }
+            }
+
+//            将当前温度值 保存到上一次温度
+            lastTemp.update(value.getTemperature());
+        }
     }
 }
